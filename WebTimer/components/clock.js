4b5f1a8d07d9818c7a1ca5d78649d19e0793aecd
jahn08 <jahn_08@mail.ru>
2018-03-25 11:45:37 +0300

    Making some enhancements to the basic component design; removing a redundant readme file from the project.

--- WebTimer/components/clock.js
+++ WebTimer/components/clock.js
@@ -3,7 +3,16 @@ var Components = {};
 Components.clock = {
 	data() {
 		return {
-			buttons: ['Start', 'Stop', 'Circle'],
+            buttons: [{
+                name: 'Start|Stop',
+                shortcut: 'Space'
+            }, {
+                name: 'Lap',
+                shortcut: 'Ctrl'
+            }, {
+               name: 'Reset',
+               shortcut: 'Esc'
+            }],
 			time: '00:00:00'
 		};
 	},
@@ -17,7 +26,9 @@ Components.clock = {
             <div class="col"></div>
             <div class="col-8 text-center">
                 <div class="btn-group btn-group-lg">
-                    <button v-for="button in buttons" type="button" class="btn btn-outline-primary"><span>{{ button }}</span></button>
+                    <button v-for="button in buttons" :key="button.name" type="button" class="btn btn-outline-primary" style="white-space:pre-wrap">
+                        <span>{{ button.name }} \n<span style='opacity:0.6'>press &lt;{{ button.shortcut }}&gt;</span></span>
+                    </button>
                 </div>
             </div>
             <div class="col"></div>
