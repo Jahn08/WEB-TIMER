82c6677e17385ba1cf3e98eac9235120f0fca0da
jahn08 <jahn_08@mail.ru>
2018-08-18 18:44:49 +0300

    Reading settings from environment variables; adding infrustructure for obtaining sensitive data from secrets.

--- WebTimer/config.js
+++ WebTimer/config.js
@@ -1,23 +1,41 @@
 const URL = require('url').URL;
 
+const fs = require('fs');
+
+const getSecret = (name) => {
+    try {
+        return fs.readFileSync('/run/secrets/' + name, {
+            encoding: 'utf8'
+        }).trim();
+    }
+    catch (e) {
+        return false;
+    }
+};
+
+const getMongoHost = () => {
+    const host = process.env.MONGO_HOST;
+    return host && host.search(/\/$/, '') === -1 ? host + '/' : host;
+};
+
 module.exports = {
     db: {
-        uri: 'mongodb://localhost:27017/WebTimer',
-        testUri: 'mongodb://localhost:27017/TestDb'
+        uri: getMongoHost() + 'WebTimer',
+        testUri: getMongoHost() + 'TestDb'
     },
     auth: {
         facebook: {
-            clientId: '',
-            clientSecret: ''
+            clientId: getSecret('AUTH_FACEBOOK_CLIENT_ID') || process.env.AUTH_FACEBOOK_CLIENT_ID,
+            clientSecret: getSecret('AUTH_FACEBOOK_CLIENT_SECRET') || process.env.AUTH_FACEBOOK_CLIENT_SECRET
         }
     },
     server: {
         pfx: {
-            path: '',
-            password: ''
+            path: process.env.SERVER_PFX_PATH,
+            password: getSecret('SERVER_PFX_PASSWORD') || process.env.SERVER_PFX_PASSWORD
         },
-        port: 3443,
-        host: 'localhost',
+        port: process.env.SERVER_PORT,
+        host: process.env.SERVER_HOST,
         getFullUrl: function () {
             const protocol = this.useHttpsProtocol() ? 'https' : 'http';
 
@@ -29,12 +47,12 @@ module.exports = {
         }
     },
     mail: {
-        host: '',
-        port: 465,
+        host: process.env.MAIL_HOST,
+        port:  process.env.MAIL_SECURE_PORT,
         secure: true,
         auth: {
-            user: '',
-            pass: ''
+            user: getSecret('MAIL_AUTH_USER') || process.env.MAIL_AUTH_USER,
+            pass: getSecret('MAIL_AUTH_PASSWORD') || process.env.MAIL_AUTH_PASSWORD
         }
     }
 };
