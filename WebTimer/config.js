f43aa45b24e6798eda6fb18fea28287feb102e74
Jahn08 <jahn_08@mail.ru>
2020-03-09 13:33:37 +0300

    The mongo host can be set through secrets; turning off SSL by environment variables.

--- WebTimer/config.js
+++ WebTimer/config.js
@@ -11,45 +11,69 @@ const getSecret = (name) => {
     }
 };
 
-const getMongoHost = () => {
-    const host = process.env.MONGO_HOST || 'mongodb://localhost:27017/';
-    return host && host.search(/\/$/, '') === -1 ? host + '/' : host;
+const envs = process.env;
+
+const getMongoHost = dbName => {
+    const host = (getSecret('MONGO_HOST') || envs.MONGO_HOST || 
+        'mongodb://localhost:27017/').trim();
+
+    const uri = host.search(/\/$/, '') === -1 ? host + '/' : host;
+
+    return `${uri}${dbName}?retryWrites=true&w=majority`;
 };
 
 const Logger = require('./tools/logger');
 
 const WebAddress = require('./tools/web-address');
 
-module.exports = {
+const parseBoolean = val => {
+    try {
+        val = JSON.parse((val || '').toLowerCase());
+
+        if (Number.isInteger(val))
+            return val !== 0;
+                    
+        return val === true;
+    }
+    catch(err) {
+        return false;
+    }
+};
+
+const config = {
     db: {
-        uri: getMongoHost() + 'WebTimer',
-        testUri: getMongoHost() + 'TestDb'
+        uri: getMongoHost('WebTimerDb'),
+        testUri: getMongoHost('TestDb')
     },
     auth: {
         facebook: {
-            clientId: getSecret('AUTH_FACEBOOK_CLIENT_ID') || process.env.AUTH_FACEBOOK_CLIENT_ID,
-            clientSecret: getSecret('AUTH_FACEBOOK_CLIENT_SECRET') || process.env.AUTH_FACEBOOK_CLIENT_SECRET
+            clientId: getSecret('AUTH_FACEBOOK_CLIENT_ID') || envs.AUTH_FACEBOOK_CLIENT_ID,
+            clientSecret: getSecret('AUTH_FACEBOOK_CLIENT_SECRET') || envs.AUTH_FACEBOOK_CLIENT_SECRET
         }
     },
     server: {
         pfx: {
-            path: process.env.SERVER_PFX_PATH || '1.pfx',
-            password: getSecret('SERVER_PFX_PASSWORD') || process.env.SERVER_PFX_PASSWORD
+            path: envs.SERVER_PFX_PATH || '1.pfx',
+            password: getSecret('SERVER_PFX_PASSWORD') || envs.SERVER_PFX_PASSWORD
         },
-        url: new WebAddress(process.env.SERVER_HOST || '0.0.0.0', process.env.SERVER_PORT || 3443),
-        externalUrl: new WebAddress(process.env.SERVER_EXTERNAL_HOST, process.env.SERVER_EXTERNAL_PORT)
+        url: new WebAddress(envs.SERVER_HOST || '0.0.0.0', envs.SERVER_PORT || 3443, 
+            parseBoolean(envs.SERVER_USE_HTTP)),
+        externalUrl: new WebAddress(envs.SERVER_EXTERNAL_HOST, envs.SERVER_EXTERNAL_PORT,
+            parseBoolean(envs.SERVER_EXTERNAL_USE_HTTP))
     },
     mail: {
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_SECURE_PORT || 465,
+        host: envs.MAIL_HOST,
+        port: envs.MAIL_SECURE_PORT || 465,
         secure: true,
         auth: {
-            user: process.env.MAIL_AUTH_USER,
-            pass: getSecret('MAIL_AUTH_PASSWORD') || process.env.MAIL_AUTH_PASSWORD
+            user: envs.MAIL_AUTH_USER,
+            pass: getSecret('MAIL_AUTH_PASSWORD') || envs.MAIL_AUTH_PASSWORD
         }
     },
     about: {
-        website: process.env.ABOUT_WEBSITE
+        website: envs.ABOUT_WEBSITE
     },
-    logger: new Logger(process.env.LOGGER_LEVEL || 'error') // warn, info
+    logger: new Logger(envs.LOGGER_LEVEL || 'error') // warn, info
 };
+
+module.exports = config;
