45b31fd064de5a10b4e4bf62316eb4d49a69692d
Jahn08 <jahn_08@mail.ru>
2020-04-25 20:21:12 +0300

    Preparing the list of default sounds before exporting them.

--- WebTimer/models/default-sounds.js
+++ WebTimer/models/default-sounds.js
@@ -1,18 +1,18 @@
+const defaultSounds = [
+    { name: 'classic alarm', file: 'alarm_classic.mp3' },
+    { name: 'alert', file: 'alert_8bit.mp3' },
+    { name: 'official bell', file: 'bell_official.mp3' },
+    { name: 'horn', file: 'horn.mp3' },
+    { name: 'horn (wahwah)', file: 'horn_wahwah.mp3' },
+    { name: 'beep', file: 'beep.mp3', internal: true }
+].sort((a, b) => a.name > b.name);
+
+const basicUrl = '../resources/audio/';
+defaultSounds.forEach(s => s.file = basicUrl + s.file);
+
 function getDefaultSounds(chosenSoundName = null) {
-    const basicUrl = '../resources/audio/';
-
-    const defaultSounds = [
-        { name: 'classic alarm', file: 'alarm_classic.mp3' },
-        { name: 'alert', file: 'alert_8bit.mp3' },
-        { name: 'official bell', file: 'bell_official.mp3' },
-        { name: 'horn', file: 'horn.mp3' },
-        { name: 'horn (wahwah)', file: 'horn_wahwah.mp3' },
-        { name: 'beep', file: 'beep.mp3', internal: true }
-    ].sort((a, b) => a.name > b.name);
-
     defaultSounds.forEach(val => {
         val.active = val.name === chosenSoundName;
-        val.file = basicUrl + val.file;
     });
 
     return defaultSounds;
